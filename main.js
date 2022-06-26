eec9b35318828561a69e490a80ec60fc8f5bc9ce
DerekYeeter <alex.robi.2020@gmail.com>
2022-06-26 19:25:18 -0400

    basic goals achieved; moving onto implementing an animation that increases in speed or changes drastically when the user clicks faster

--- main.js
+++ main.js
@@ -12,9 +12,24 @@ function turn()
 function increment()
 {
    clicks++;
-   let counter = document.querySelector('span');
-   if (counter == 15)
+   
+   if (clicks >= 15)
    {
+       let wind = document.querySelector('wind');
+       wind.textContent = "\nGust of Wind Unlocked!"
    }
-   counter.textContent = clicks;
+   
+   if (clicks >= 15)
+   {
+       let panel = document.querySelector('panel');
+       panel.textContent = "Solar Panel Unlocked!"
+   }
+
+   if (clicks >= 15)
+   {
+       let house = document.querySelector('house');
+       house.textContent = "You can now power 1 american home for [insert hours here] hours !"
+   }
+
+   document.querySelector('span').textContent = clicks;
 }
